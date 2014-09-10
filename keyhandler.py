88800c2c703e18d2f7f7795ba83b96d0fa5298c0
Plamen Mishev <pmishev@gmail.com>
2014-09-10 23:48:36 +0000

    Initial commit

---
+++ keyhandler.py
@@ -0,0 +1,13 @@
+import sys, tty, termios
+
+# Gets a single character from standard input.
+def getKey():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
