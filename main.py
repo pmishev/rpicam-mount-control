77f4d00b1aaede1fdeb16b6de5e810a3f135f9ab
Plamen Mishev <pmishev@gmail.com>
2014-09-12 22:43:00 +0000

    experimenting with threads

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-import thread
+import threading
 import time
 import sys
+import RPi.GPIO as GPIO
 import keyhandler
 
 waitTime = 0.001
@@ -14,28 +15,43 @@ char = None;
 def keypressDetector():
 	global char
 	while True:
-		char = keyhandler.getKey()
-		time.sleep(0.005) # Bad things happen for some reason if there's no sleep at all
+		#try:
+			char = keyhandler.getKey()
+			time.sleep(0.005) # Bad things happen for some reason if there's no sleep at all
+		#except:
+			# Sometimes after the main program terminates, the keyhandler object is destroyed before this thread has finished
+			# We catch the exception to avoid outputting ugly error message in that case
+		#	pass
 
-# Start the keypress detector in a separate thread, so it doesn't block the main program execution
-thread.start_new_thread(keypressDetector, ())
 
-# Start the main loop
-while True:
-	if char is not None:
-		#print "Key pressed is: {}".format(char)
-		if char == 'q':
-			sys.exit()
-		elif char == 'a':
-			print "Go left"
-		elif char == 'd':
-			print "Go right"
-		elif char == 'w':
-			print "Go up"
-		elif char == 's':
-			print "Go down"
 
-		char = None
+try:
+	# Start the keypress detector in a separate thread, so it doesn't block the main program execution
+	#thread.start_new_thread(keypressDetector, ())
+	keyHandlerThread = threading.Thread(target=keypressDetector, name="keyHandler")
+	keyHandlerThread.start()
 
-	time.sleep(waitTime)
+	# Start the main loop
+	while True:
+		if char is not None:
+			#print "Key pressed is: {}".format(char)
+			if char == 'q':
+				sys.exit()
+			elif char == 'a':
+				print "Go left"
+			elif char == 'd':
+				print "Go right"
+			elif char == 'w':
+				print "Go up"
+			elif char == 's':
+				print "Go down"
+
+			char = None
+
+		time.sleep(waitTime)
+
+# Stop on Ctrl+C and clean up
+except (KeyboardInterrupt, SystemExit):
+	print "Exiting"
+	#GPIO.cleanup()
 
